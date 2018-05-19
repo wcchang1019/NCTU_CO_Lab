32c215126e54c5fa2977ee80281fbacb855cc271
smallwhite <destiny10191019@gmail.com>
2018-05-19 13:45:11 +0800

    add signed

--- Lab3/ALU.v
+++ Lab3/ALU.v
@@ -19,8 +19,8 @@ module ALU(
 	);
      
 //I/O ports
-input  [32-1:0]  src1_i;
-input  [32-1:0]	 src2_i;
+input signed [32-1:0]  src1_i;
+input signed [32-1:0]  src2_i;
 input  [4-1:0]   ctrl_i;
 
 output [32-1:0]	 result_o;
